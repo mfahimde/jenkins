c0db7bf311010bcdde96a33cbe9e0d858fb2924d
mfahimde <mohammad.devin23@gmail.com>
2021-03-06 11:26:45 -0500

    my first commit

---
+++ src/jenkins/jenk1.java
@@ -0,0 +1,10 @@
+package jenkins;
+
+public class jenk1 {
+    public static void main(String[] args) throws InterruptedException {
+        for(int i=0;i<5;i++){
+            System.out.println("hi momo");
+            Thread.sleep(2000);
+        }
+    }
+}
