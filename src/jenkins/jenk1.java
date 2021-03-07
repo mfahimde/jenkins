f8b9b0772799bae3845b697e5a10572e1d4e19ad
mfahimde <mohammad.devin23@gmail.com>
2021-03-06 19:55:15 -0500

    fisrt11

--- src/jenkins/jenk1.java
+++ src/jenkins/jenk1.java
@@ -3,7 +3,7 @@ package jenkins;
 public class jenk1 {
     public static void main(String[] args) throws InterruptedException {
         for(int i=0;i<5;i++){
-            System.out.println("hi momo3");
+            System.out.println("hi momo3 sout");
             Thread.sleep(2000);
         }
     }
